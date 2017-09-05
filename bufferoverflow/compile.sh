c07bc523d6574bd503bc76b8c433d520b485da82
Alexandre R Gomes <gomes.bcc@gmail.com>
2017-09-05 16:30:33 -0300

    Expliciting sudo and cut off tee prompt

--- bufferoverflow/compile.sh
+++ bufferoverflow/compile.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # remember to put '0' in randomize_va_space. CTRL+D after type '0'
-sudo tee /proc/sys/kernel/randomize_va_space
+echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
 # and to compile with -fno-stack-protector
-gcc stackoverflow.c -o exec -fno-stack-protector
+#gcc stackoverflow.c -o exec -fno-stack-protector
