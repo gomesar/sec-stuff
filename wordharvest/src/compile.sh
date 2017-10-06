beda1877b9b310f68a11a04b3279cd72d97e6a6b
Alexandre R Gomes <gomes.bcc@gmail.com>
2017-10-06 18:48:49 -0300

    Example added. Dummy files available. Compile updated.

--- wordharvest/src/compile.sh
+++ wordharvest/src/compile.sh
@@ -6,5 +6,8 @@ fi
 gcc wordharvest.c -o ../bin/wordharvest
 gcc bruteforce.c -o ../bin/bruteforce
 
-#../test/_test.sh
+# Comment the lines below to not generate dummy files and tip
+unzip -o dummy.zip -d ../
+echo "[!] Go to ../test/ and run ./_test.sh"
+#cd ../test/; ./_test.sh; cd ../src;
 
--- wordharvest/test/_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-../bin//wordharvest -d "./" -o wordlist -e "text:txt:asc"
-../bin/bruteforce -l wordlist -f file.zip
